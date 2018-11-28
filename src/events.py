1b39d3078a1e028340cb63ede5f8ef922a0d0ba9
Cole Ramey <ceramey1997@gmail.com>
2018-11-28 16:11:39 -0600

    fixed album search

--- src/events.py
+++ src/events.py
@@ -17,6 +17,9 @@ class Event_Message:
 
         if message.content.startswith('!join'):
             await self.join(client, message)
+        
+        if message.content.startswith('!help'):
+            await self.message_help(client, message)
 
     async def message_hello(self, client, message):
         msg = 'Hello {0.author.mention}'.format(message)
@@ -25,10 +28,11 @@ class Event_Message:
     async def message_play(self, client, message):
         album_artist= message.content.replace("!play ", "")
         album, artist = album_artist.split(",")
+        import pdb; pdb.set_trace()
         album = album.strip()
         artist = artist.strip()
-        album = spotify_object.get_album(message)
-        song_queue[message.content[5:]] =  'url'
+        album_info = spotify_object.get_album(album, artist)
+        song_queue[message.content[5:]] =  album_info
         msg = '"' + message.content[5:] + '" has been added to the song queue'
         await client.send_message(message.channel, msg)
 
@@ -44,3 +48,9 @@ class Event_Message:
     async def join(self, client, message):
         channel = client.get_channel('501955815222149154')
         await client.join_voice_channel(channel)
+
+    async def help(self, client, message):
+        message = "!play - plays an album. input is as \"!play album,artist\""
+        message = message + "\n!queue - returns the music queue"
+        message = message + "\n!join - joins the general channel\n!hello - says hello back"
+        await client.send_message(message.channel, message)
