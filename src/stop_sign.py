9efcbf8b369f9d93f1e9c5f41fcc9405be2a5240
tbeisel002 <tbeisel002@gmail.com>
2018-11-29 16:20:59 -0600

    added stop sign class

---
+++ src/stop_sign.py
@@ -0,0 +1,9 @@
+class Stop_Sign:
+    def __init__(self, flag):
+        self.flag = False
+
+    def get_flag(self):
+        return self.flag
+
+    def set_flag(self, new):
+        self.flag = new
